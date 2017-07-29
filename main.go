4b50268f4b308e04fa59d2336573d84668b6091e
Benjamin Lannon <lannonbr@clarkson.edu>
2017-07-28 22:26:49 -0400

    Added different formats

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -64,19 +65,17 @@ func analyzeFile(filename string, fullPrint bool) (uint64, uint64, uint64, uint6
 		totalSend += entry.Send
 	}
 
-	if fullPrint {
-		fmt.Println("Filename:", filename)
-	}
-
-	humanRecv := humanize.Bytes(totalRecv)
-	humanSend := humanize.Bytes(totalSend)
-
 	totalOverall := totalRecv + totalSend
-	humanOverall := humanize.Bytes(totalOverall)
 
 	rate := ((totalRecv + totalSend) * 8) / 3550
 
 	if fullPrint {
+		humanRecv := humanize.Bytes(totalRecv)
+		humanSend := humanize.Bytes(totalSend)
+		humanOverall := humanize.Bytes(totalOverall)
+
+		fmt.Println("Filename:", filename)
+
 		fmt.Println("Total Received:", humanRecv)
 		fmt.Println("Total Sent:", humanSend)
 		fmt.Println("Total overall:", humanOverall)
@@ -87,9 +86,37 @@ func analyzeFile(filename string, fullPrint bool) (uint64, uint64, uint64, uint6
 	return totalRecv, totalSend, totalOverall, rate
 }
 
+func prettyPrint(date string, totalRecv, totalSend, totalOverall, avgRate uint64) {
+	fmt.Printf("Timestamp: %v\n", date)
+	fmt.Println("------------")
+	fmt.Println("Recieved:", humanize.Bytes(totalRecv))
+	fmt.Println("Sent:", humanize.Bytes(totalSend))
+	fmt.Println("Overall:", humanize.Bytes(totalOverall))
+	fmt.Println("Rate:", humanizeBits(avgRate)+"/sec")
+}
+
+func csvPrint(date string, totalRecv, totalSend, totalOverall, avgRate uint64) {
+	fmt.Printf("%s,%s,%s,%s,%s\n", date, humanize.Bytes(totalRecv), humanize.Bytes(totalSend), humanize.Bytes(totalOverall), humanizeBits(avgRate)+"/sec")
+}
+
+func csvPrintRaw(date string, totalRecv, totalSend, totalOverall, avgRate uint64) {
+	fmt.Printf("%s,%d,%d,%d,%d", date, totalRecv, totalSend, totalOverall, avgRate)
+}
+
 func main() {
 
-	files := os.Args[1:]
+	format := os.Args[1]
+	files := os.Args[2:]
+
+	dateRegex, _ := regexp.Compile("([A-Z][a-z]{2,3})-([\\d]{1,2})-([\\d]{4})_([\\d]{2})")
+
+	arr := dateRegex.FindStringSubmatch(files[0])
+
+	month, day, year, hour := arr[1], arr[2], arr[3], arr[4]
+
+	date := fmt.Sprintf("%s/%s/%s", month, day, year)
+	dateWithHour := fmt.Sprintf("%s/%s/%s %s:00", month, day, year, hour)
+	dateMonth := fmt.Sprintf("%s/%s", month, year)
 
 	var counter int
 
@@ -108,9 +135,28 @@ func main() {
 
 	avgRate = totalRate / uint64(counter)
 
-	fmt.Println("Recieved:", humanize.Bytes(totalRecv))
-	fmt.Println("Sent:", humanize.Bytes(totalSend))
-	fmt.Println("Overall:", humanize.Bytes(totalOverall))
-	fmt.Println("Rate:", humanizeBits(avgRate)+"/sec")
+	switch format {
+	//Pretty Printed Formats
+	case "pretty_month":
+		prettyPrint(dateMonth, totalRecv, totalSend, totalOverall, avgRate)
+	case "pretty_day":
+		prettyPrint(date, totalRecv, totalSend, totalOverall, avgRate)
+	case "pretty_hour":
+		prettyPrint(dateWithHour, totalRecv, totalSend, totalOverall, avgRate)
+	// CSV Formats
+	case "csv_month":
+		csvPrint(dateMonth, totalRecv, totalSend, totalOverall, avgRate)
+	case "csv_day":
+		csvPrint(date, totalRecv, totalSend, totalOverall, avgRate)
+	case "csv_hour":
+		csvPrint(dateWithHour, totalRecv, totalSend, totalOverall, avgRate)
+	// Raw CSV (No humanized entries)
+	case "csv_month_raw":
+		csvPrintRaw(dateMonth, totalRecv, totalSend, totalOverall, avgRate)
+	case "csv_day_raw":
+		csvPrintRaw(date, totalRecv, totalSend, totalOverall, avgRate)
+	case "csv_hour_raw":
+		csvPrintRaw(dateWithHour, totalRecv, totalSend, totalOverall, avgRate)
+	}
 
 }
